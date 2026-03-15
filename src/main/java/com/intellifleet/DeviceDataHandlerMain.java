6cd8848687f42f2291edfaa1dea147554c46f4d0
Pradyut Sarkar <pradyut.bca15@gmail.com>
2026-03-15 08:33:39 +0530

    code changes

--- src/main/java/com/intellifleet/DeviceDataHandlerMain.java
+++ src/main/java/com/intellifleet/DeviceDataHandlerMain.java
@@ -2,8 +2,9 @@ package com.intellifleet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = "com.intellifleet")
 public class DeviceDataHandlerMain {
     public static void main(String[] args) {
